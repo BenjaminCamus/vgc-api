12323eabb8b8ae2ee4f4ff682c2da340123db504
Ben <benjamin@camus.me>
2017-12-08 19:11:17 +0100

    fix igdb search

--- src/GameBundle/Controller/IgdbController.php
+++ src/GameBundle/Controller/IgdbController.php
@@ -11,6 +11,7 @@ namespace GameBundle\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
+use GameBundle\Entity\Platform;
 use GameBundle\Utils\IGDB;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,8 +25,66 @@ class IgdbController extends FOSRestController
      */
     public function getSearchAction($search)
     {
+        $em = $this->getDoctrine()->getManager();
         $igdbService = $this->container->get('igdb');
         $igdb = $igdbService->get('games/?search=' . $search . '&fields=*&limit=' . 10);
+
+        $platforms = [];
+
+        foreach ($igdb as $igdbGame) {
+
+            if (!isset($igdbGame->cover) || !isset($igdbGame->platforms)) {
+                unset($igdbGame);
+                continue;
+            }
+
+            $igdbGame->cover->cloudinaryId = $igdbGame->cover->cloudinary_id;
+
+            if (isset($igdbGame->screenshots) && count($igdbGame->screenshots) > 0) {
+                foreach ($igdbGame->screenshots as $screenshot) {
+                    $screenshot->cloudinaryId = $screenshot->cloudinary_id;
+                }
+            }
+
+            $igdbGamePlatforms = [];
+
+            foreach ($igdbGame->platforms as $platformId) {
+
+                if (array_key_exists($platformId, $platforms)) {
+                    $igdbGamePlatforms[] = $platforms[$platformId];
+                } else {
+                    $platformRepository = $this->getDoctrine()->getRepository('GameBundle:Platform');
+                    $platform = $platformRepository->findOneByIgdbId($platformId);
+
+                    if (is_null($platform)) {
+
+                        // Platform not in db : new Platform
+                        $platform = new Platform();
+                        $platform->setIgdbId($platformId);
+
+                        // Get IGDB game
+                        $igdbPlatforms = $igdbService->get('platforms/' . $platform->getIgdbId() . '?fields=*');
+                        $igdbPlatform = $igdbPlatforms[0];
+
+                        $igdbGamePlatforms[] = $igdbPlatform;
+
+                        $platform->setName($igdbPlatform->name);
+                        $platform->setIgdbUrl($igdbPlatform->url);
+
+                        $em->persist($platform);
+                        $em->flush();
+                    } else {
+                        $igdbPlatform = new \stdClass();
+                        $igdbPlatform->id = $platform->getIgdbId();
+                        $igdbPlatform->name = $platform->getName();
+
+                        $igdbGamePlatforms[] = $igdbPlatform;
+                    }
+                }
+            }
+
+            $igdbGame->platforms = $igdbGamePlatforms;
+        }
 
         return new JsonResponse($igdb);
     }
