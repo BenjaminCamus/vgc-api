7aac6344d62883a42ba34f7f9e14eb6af455083b
Ben <benjamin@camus.me>
2018-03-19 20:50:14 +0100

    users admin

---
+++ src/UserBundle/Admin/UserAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace UserBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class UserAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('username')
+            ->add('email')
+            ->add('roles')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('passwordRequestedAt');
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('username')
+            ->add('email')
+            ->add('roles')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('passwordRequestedAt')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                ),
+            ));
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('username')
+            ->add('email')
+            ->add('roles')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('passwordRequestedAt');
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('username')
+            ->add('email')
+            ->add('roles')
+            ->add('enabled')
+            ->add('lastLogin')
+            ->add('passwordRequestedAt');
+    }
+}
